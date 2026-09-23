dafb75d82f40bccbe43e4450a9b1471c764623b0
CelianGuillamet <celian.guillamet@gmail.com>
2026-09-23 15:00:59 +0200

    docs: record single-ticket resumption and final review

--- api/prisma/migrations/20260922160000_application_identity/migration.sql
+++ api/prisma/migrations/20260922160000_application_identity/migration.sql
@@ -90,4 +90,3 @@ ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
